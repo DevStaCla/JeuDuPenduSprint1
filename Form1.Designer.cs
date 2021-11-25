7f746671f0b319358e3bcbd4689e2cd1fa6df63d
Claire <claire.stalter@gmail.com>
2021-11-25 09:04:10 +0100

    Procédures de gestion de phases

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -43,7 +43,10 @@ namespace JeuDuPendu
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.lblMysteryWord = new System.Windows.Forms.Label();
+            this.lblInputConfirm = new System.Windows.Forms.Label();
+            this.lblLettersEntered = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.grpLetters.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -57,11 +60,13 @@ namespace JeuDuPendu
             // 
             // cbbAlphabet
             // 
+            this.cbbAlphabet.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbbAlphabet.FormattingEnabled = true;
-            this.cbbAlphabet.Location = new System.Drawing.Point(167, 113);
+            this.cbbAlphabet.Location = new System.Drawing.Point(194, 113);
             this.cbbAlphabet.Name = "cbbAlphabet";
-            this.cbbAlphabet.Size = new System.Drawing.Size(40, 21);
+            this.cbbAlphabet.Size = new System.Drawing.Size(40, 26);
             this.cbbAlphabet.TabIndex = 3;
+            this.cbbAlphabet.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cbbAlphabet_KeyPress);
             // 
             // button1
             // 
@@ -76,8 +81,8 @@ namespace JeuDuPendu
             // 
             this.lblWinner.AutoSize = true;
             this.lblWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblWinner.ForeColor = System.Drawing.Color.Crimson;
-            this.lblWinner.Location = new System.Drawing.Point(53, 368);
+            this.lblWinner.ForeColor = System.Drawing.Color.Goldenrod;
+            this.lblWinner.Location = new System.Drawing.Point(49, 368);
             this.lblWinner.Name = "lblWinner";
             this.lblWinner.Size = new System.Drawing.Size(264, 31);
             this.lblWinner.TabIndex = 5;
@@ -86,70 +91,82 @@ namespace JeuDuPendu
             // lblPlayer1
             // 
             this.lblPlayer1.AutoSize = true;
-            this.lblPlayer1.Location = new System.Drawing.Point(22, 17);
+            this.lblPlayer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1.Location = new System.Drawing.Point(21, 14);
             this.lblPlayer1.Name = "lblPlayer1";
-            this.lblPlayer1.Size = new System.Drawing.Size(60, 13);
+            this.lblPlayer1.Size = new System.Drawing.Size(101, 24);
             this.lblPlayer1.TabIndex = 6;
             this.lblPlayer1.Text = "JOUEUR 1";
             // 
             // lblPlayer2
             // 
             this.lblPlayer2.AutoSize = true;
-            this.lblPlayer2.Location = new System.Drawing.Point(22, 98);
+            this.lblPlayer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer2.Location = new System.Drawing.Point(21, 90);
             this.lblPlayer2.Name = "lblPlayer2";
-            this.lblPlayer2.Size = new System.Drawing.Size(60, 13);
+            this.lblPlayer2.Size = new System.Drawing.Size(101, 24);
             this.lblPlayer2.TabIndex = 7;
             this.lblPlayer2.Text = "JOUEUR 2";
             // 
             // txtPly1
             // 
-            this.txtPly1.Location = new System.Drawing.Point(167, 32);
+            this.txtPly1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPly1.Location = new System.Drawing.Point(194, 40);
             this.txtPly1.Name = "txtPly1";
-            this.txtPly1.Size = new System.Drawing.Size(184, 20);
+            this.txtPly1.Size = new System.Drawing.Size(162, 24);
             this.txtPly1.TabIndex = 8;
+            this.txtPly1.Click += new System.EventHandler(this.txtPly1_Click);
+            this.txtPly1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPly1_KeyPress);
             // 
             // grpLetters
             // 
-            this.grpLetters.Location = new System.Drawing.Point(35, 248);
+            this.grpLetters.Controls.Add(this.lblLettersEntered);
+            this.grpLetters.Location = new System.Drawing.Point(25, 277);
             this.grpLetters.Name = "grpLetters";
-            this.grpLetters.Size = new System.Drawing.Size(278, 84);
+            this.grpLetters.Size = new System.Drawing.Size(296, 55);
             this.grpLetters.TabIndex = 9;
             this.grpLetters.TabStop = false;
             this.grpLetters.Text = "Lettres déjà proposées";
             // 
             // btnPly2Confirm
             // 
-            this.btnPly2Confirm.Location = new System.Drawing.Point(213, 113);
+            this.btnPly2Confirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPly2Confirm.Location = new System.Drawing.Point(240, 113);
             this.btnPly2Confirm.Name = "btnPly2Confirm";
-            this.btnPly2Confirm.Size = new System.Drawing.Size(88, 21);
+            this.btnPly2Confirm.Size = new System.Drawing.Size(68, 26);
             this.btnPly2Confirm.TabIndex = 10;
             this.btnPly2Confirm.Text = "Valider";
             this.btnPly2Confirm.UseVisualStyleBackColor = true;
+            this.btnPly2Confirm.Click += new System.EventHandler(this.btnPly2Confirm_Click);
             // 
             // btnPly1Confirm
             // 
-            this.btnPly1Confirm.Location = new System.Drawing.Point(357, 32);
+            this.btnPly1Confirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPly1Confirm.Location = new System.Drawing.Point(362, 40);
             this.btnPly1Confirm.Name = "btnPly1Confirm";
-            this.btnPly1Confirm.Size = new System.Drawing.Size(88, 20);
+            this.btnPly1Confirm.Size = new System.Drawing.Size(68, 25);
             this.btnPly1Confirm.TabIndex = 11;
             this.btnPly1Confirm.Text = "Valider";
             this.btnPly1Confirm.UseVisualStyleBackColor = true;
+            this.btnPly1Confirm.Click += new System.EventHandler(this.btnPly1Confirm_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 39);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 44);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.Size = new System.Drawing.Size(171, 20);
             this.label1.TabIndex = 12;
             this.label1.Text = "Entre le mot à deviner :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(22, 121);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.Size = new System.Drawing.Size(147, 20);
             this.label2.TabIndex = 13;
             this.label2.Text = "Propose une lettre :";
             // 
@@ -157,17 +174,38 @@ namespace JeuDuPendu
             // 
             this.lblMysteryWord.AutoSize = true;
             this.lblMysteryWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMysteryWord.Location = new System.Drawing.Point(42, 188);
+            this.lblMysteryWord.Location = new System.Drawing.Point(51, 211);
             this.lblMysteryWord.Name = "lblMysteryWord";
             this.lblMysteryWord.Size = new System.Drawing.Size(230, 24);
             this.lblMysteryWord.TabIndex = 14;
             this.lblMysteryWord.Text = "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _";
+            this.lblMysteryWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblInputConfirm
+            // 
+            this.lblInputConfirm.AutoSize = true;
+            this.lblInputConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInputConfirm.ForeColor = System.Drawing.Color.Red;
+            this.lblInputConfirm.Location = new System.Drawing.Point(433, 46);
+            this.lblInputConfirm.Name = "lblInputConfirm";
+            this.lblInputConfirm.Size = new System.Drawing.Size(0, 16);
+            this.lblInputConfirm.TabIndex = 15;
+            // 
+            // lblLettersEntered
+            // 
+            this.lblLettersEntered.AutoSize = true;
+            this.lblLettersEntered.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLettersEntered.Location = new System.Drawing.Point(12, 26);
+            this.lblLettersEntered.Name = "lblLettersEntered";
+            this.lblLettersEntered.Size = new System.Drawing.Size(0, 18);
+            this.lblLettersEntered.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(592, 423);
+            this.Controls.Add(this.lblInputConfirm);
             this.Controls.Add(this.lblMysteryWord);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -185,6 +223,8 @@ namespace JeuDuPendu
             this.Text = "Jeu du pendu";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.grpLetters.ResumeLayout(false);
+            this.grpLetters.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -205,6 +245,8 @@ namespace JeuDuPendu
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lblMysteryWord;
+        private System.Windows.Forms.Label lblInputConfirm;
+        private System.Windows.Forms.Label lblLettersEntered;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,17 +17,116 @@ namespace JeuDuPendu
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        /// <summary>
+        /// lance la phase 1 du programme (J1 choisit un mot)
+        /// </summary>
+        private void Phase1()
         {
+            // initialisation de la liste contenant l'alphabet
             RemplitCombo(cbbAlphabet);
+            // désactivation de la liste et du bouton du J2
+            cbbAlphabet.Enabled = false;
+            btnPly2Confirm.Enabled = false;
+            // focus sur le champ du J1
+            txtPly1.Select();
         }
 
-        static void RemplitCombo(ComboBox liste)
+        /// <summary>
+        /// lance la phase 2 du programme (J2 devine le mot)
+        /// </summary>
+        private void Phase2(char[] lettres)
+        {
+            // désactivation du champ et du bouton du J1
+            txtPly1.Enabled = false;
+            btnPly1Confirm.Enabled = false;
+            // activation de la liste et du bouton du J2
+            cbbAlphabet.Enabled = true;
+            cbbAlphabet.Focus();
+            btnPly2Confirm.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Phase1();
+        }
+
+        private void RemplitCombo(ComboBox liste)
         {
             liste.Items.Clear();
             for (int i = 0; i < 26; i++)
             {
                 liste.Items.Add((char)('A' + i));
+            }
+        }
+
+        private void btnPly1Confirm_Click(object sender, EventArgs e)
+        {
+            String mot = txtPly1.Text.ToUpper();
+            char[] lettres = new char[mot.Length];
+            int i = 0;
+            bool correct = true;
+            if (mot.Length == 0 || mot.Length > 15)
+            {
+                lblInputConfirm.ForeColor = Color.Red;
+                lblInputConfirm.Text = "Tape entre 1 et 15 lettres";
+            }
+            else
+            {
+                while (correct && i < mot.Length)
+                {
+                    int test = mot[i];
+                    if (mot[i] >= 65 && mot[i] <= 90)
+                    {
+                        lettres[i] = mot[i];
+                        i++;
+                    }
+                    else
+                    {
+                        correct = false;
+                    }
+                }
+                if (correct)
+                {
+                    lblInputConfirm.ForeColor = Color.Green;
+                    lblInputConfirm.Text = "Mot enregistré";
+                    Phase2(lettres);
+                }
+                else
+                {
+                    lblInputConfirm.ForeColor = Color.Red;
+                    lblInputConfirm.Text = "Caractère interdit";
+                }
+            }            
+        }
+
+        private void txtPly1_Click(object sender, EventArgs e)
+        {
+            lblInputConfirm.Text = "";
+        }
+
+        private void txtPly1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\r')
+            {
+                btnPly1Confirm_Click(null, null);
+            }
+            else
+            {
+                txtPly1_Click(null, null);
+            }
+        }
+
+        private void btnPly2Confirm_Click(object sender, EventArgs e)
+        {
+            lblLettersEntered.Text += cbbAlphabet.SelectedItem;
+            cbbAlphabet.Items.Remove(cbbAlphabet.SelectedItem);
+        }
+
+        private void cbbAlphabet_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\r')
+            {
+                btnPly2Confirm_Click(null, null);
             }
         }
     }
